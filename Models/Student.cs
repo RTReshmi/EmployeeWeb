81e99e20ac9242651b02223d0c6c78c85146a752
RTReshmi <rechi212@gmail.com>
2022-05-21 10:37:27 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWeb.Models
+{
+    public class Student
+    {
+        public Guid EmployeeID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ db/StudentDBWeb.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeWeb.db
+{
+    public class StudentDBWeb:DbContext
+    {
+        public StudentDBWeb(DbContextOptions<StudentDBWeb> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+    
+}
+
+}
